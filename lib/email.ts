aabe3d16ffe0052512898fbdb0a175e125d0273d
MSBHAILA <mohammedbhaila19@gmail.com>
2026-01-04 00:00:54 +0300

    update email configs.

--- lib/email.ts
+++ lib/email.ts
@@ -2,7 +2,8 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.sendgrid.net',
-    port: 25,
+    port: 587,
+    secure: false,
     auth: {
         user: 'apikey',
         pass: process.env.SENDGRID_API_KEY,
